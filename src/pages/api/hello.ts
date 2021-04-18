3eed1a6d73beebf489d5594e33174a75dc69db35
Zondazx <mzxgd16@gmail.com>
2021-04-18 19:49:15 -0400

    style: responsiveness

--- src/pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  response.status(200).json({ name: 'John Doe' });
-}
